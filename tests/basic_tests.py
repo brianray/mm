71f3b7849d6dc11e0fe78eebac882cf31ec92601
Brian Ray <brianhray@gmail.com>
2011-11-26 16:44:06 -0600

    initial tests

---
+++ tests/basic_tests.py
@@ -0,0 +1,34 @@
+import unittest
+import datetime
+import mm
+
+class TestBasicSuite(unittest.TestCase):
+
+
+    def test_minimal(self):
+
+        my_data = [ 
+            {
+                'msg': "My first Cell",
+                'id': 1,
+                'when': datetime.datetime.now(),
+            },
+            {
+                'msg': "My second Cell",
+                'id': 2,
+                'when': datetime.datetime.now(),
+            },
+
+        ]
+
+        mm_doc = mm.Document(my_data)
+        str = mm_doc.writestr()
+        self.assertTrue(len(str) > 10, 
+            msg="String should be longer than %s" % len(str))
+
+
+
+
+
+
+
