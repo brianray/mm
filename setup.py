38fc55b1f6ec40829d20198ad3130779d957cf69
Marco Braak <mbraak@ridethepony.nl>
2013-01-14 11:13:31 +0100

    Include lib package in setup. Use setuptools instead distutils.

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
-#!/usr/bin/env python
+from setuptools import setup, find_packages
 
-# ...
 
-from distutils.core import setup
-
-setup(name='Marmir',
-      version="0.1",
-      description='Marmir: makes spreadsheets easy',
-      author='Brian Ray',
-      author_email='brianhray@gmail.com',
-      maintainer='Brian Ray',
-      maintainer_email='brianhray@gmail.com',
-      url='https://github.com/brianray/mm',
-      packages=['mm', ],
-      long_description="Python power spreadsheets on steroids",
-#      license="Brian Ray", TBD
-      platforms=["any"],
-     )
-
+setup(
+    name='Marmir',
+    version="0.1",
+    description='Marmir: makes spreadsheets easy',
+    author='Brian Ray',
+    author_email='brianhray@gmail.com',
+    maintainer='Brian Ray',
+    maintainer_email='brianhray@gmail.com',
+    url='https://github.com/brianray/mm',
+    long_description="Python power spreadsheets on steroids",
+    #      license="Brian Ray", TBD
+    platforms=["any"],
+    packages=find_packages(),
+    include_package_data=True,
+)
