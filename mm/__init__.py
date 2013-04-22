d1ed00ce766756e534edd98d1cdef3c86f409c8a
Brian Ray <brianhray@gmail.com>
2013-04-21 21:44:45 -0500

    fixes #3 added basic config to loggers

--- mm/__init__.py
+++ mm/__init__.py
@@ -10,10 +10,12 @@ __license__ = "TBD"
 from document_base import *
 import config_base
 import model_base
+import logging
+logging.basicConfig()
+
 
 Date = model_base.DateFieldType
 URL = model_base.URLFieldType
 Image = model_base.ImageFieldType
 
 Config = config_base.ConfigBase
-
