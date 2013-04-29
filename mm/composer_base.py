1cc08b14ed81e1654a56f2abd34383fce8808d01
Brian Ray <brianhray@gmail.com>
2013-04-29 08:03:40 -0500

    abstract out row()/set_option() on base class

--- mm/composer_base.py
+++ mm/composer_base.py
@@ -17,16 +17,18 @@ class ComposerBase(object):
         self.row_id = 0
         self.col_id = 0
 
-    def iterate_grid(self):
+    def row(self, row):
+        self.col_id = 0
+        self.start_new_row(self.row_id)
+        for cell in row:
+            self.write_cell(self.row_id, self.col_id, cell)
+            self.col_id += 1
+        self.end_row(self.row_id)
+        self.row_id += 1
 
+    def iterate_grid(self):
         for row in self.grid.grid_data:
-            self.col_id = 0
-            self.start_new_row(self.row_id)
-            for cell in row:
-                self.write_cell(self.row_id, self.col_id, cell)
-                self.col_id += 1
-            self.end_row(self.row_id)
-            self.row_id += 1
+            self.row(row)
 
     def write_header(self):
         i = 0
@@ -36,6 +38,9 @@ class ComposerBase(object):
             self.write_cell(0, i, cell)
             i += 1
         self.row_id += 1
+
+    def set_option(self, x):
+        log.warn("%s not supported with this composer %s" % (x, self.__class__.__name__))
 
     def finish(self):
         """ Things we do after we are done """
